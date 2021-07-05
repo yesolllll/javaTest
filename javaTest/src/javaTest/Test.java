165688665062ae8d009c38a8453c776f6f2d4270
yesolllll <86939128+yesolllll@users.noreply.github.com>
2021-07-05 15:28:46 +0900

    깃허브 수정

--- javaTest/src/javaTest/Test.java
+++ javaTest/src/javaTest/Test.java
@@ -3,7 +3,8 @@ package javaTest;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("test1");
+		System.out.println("test2");
 
 	}
 
